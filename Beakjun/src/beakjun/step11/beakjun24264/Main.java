2f8e30c452b14d592ecabbb4406197c60ec16993
danbee <danb0308@naver.com>
2023-05-06 13:25:14 +0900

    beakjun24265

--- Beakjun/src/beakjun/step11/beakjun24264/Main.java
+++ Beakjun/src/beakjun/step11/beakjun24264/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		long l = sc.nextLong();
+		
 		System.out.println(l*l);
 		System.out.println(2);
 	}
---
+++ Beakjun/src/beakjun/step11/beakjun24265/Main.java
@@ -0,0 +1,16 @@
+package beakjun.step11.beakjun24265;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println((n-1)*n/2);
+		System.out.println(2);
+	}
+
+}
