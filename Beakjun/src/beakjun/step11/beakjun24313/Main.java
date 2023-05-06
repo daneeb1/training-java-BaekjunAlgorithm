b19dc405ee832530010b2b0a3a501ea624f398f4
danbee <danb0308@naver.com>
2023-05-06 22:05:05 +0900

    beakjun24313

--- Beakjun/src/beakjun/step11/beakjun24313/Main.java
+++ Beakjun/src/beakjun/step11/beakjun24313/Main.java
@@ -1,10 +1,24 @@
 package beakjun.step11.beakjun24313;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a1 = sc.nextInt();
+		int a0 = sc.nextInt();
+		
+		int c = sc.nextInt();
+		int n = sc.nextInt();
+		sc.close();
+		
+		if(((a1*n)+ a0 <= c*n) && (c >= a1)){
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+		
 	}
 
 }
